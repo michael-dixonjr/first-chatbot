a4a4e2e59aa310d61f5df32246c6996601f3b8ef
Michael Dixon <mykeydixon@gmail.com>
2022-12-24 17:11:47 -0800

    first commit

---
+++ client/script.js
@@ -0,0 +1,122 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval
+
+function loader(element) {
+    element.textContent = 'um'
+
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += 'm';
+
+        // restarts um loading sequence
+        if (element.textContent === 'ummmmm') {
+            element.textContent = 'um';
+        }
+    }, 300);
+}
+//function controlls how the text will appear on the screen when the bot is giving its answer
+function typeText(element, text) {
+    let index = 0
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
+
+//generates unique IDs for each method so that they can be mapped over
+
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+//alternating colors for chat questions and responses
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const data = new FormData(form)
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+    // to clear the textarea input 
+    form.reset()
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "umm"
+    loader(messageDiv)
+    //fetch data from the server to get the bots response
+    const response = await fetch('https://localhost:5000 ', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval)//clears the loader
+    messageDiv.innerHTML = " "//may conflict with the um loading sequence, check later <----------------!
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+        typeText(messageDiv, parsedData)
+    } else {
+        const err = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
+    }
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
+})
+
+//C:\Users\mykey\My Programs\chatbot\client\assets\bot.svg
+//C:\Users\mykey\My Programs\chatbot\client\script.js
---
+++ server/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+//a function that can accept an object in which the key is passed to
+const configuration = new Configuration ({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+//new instance of OPENAI
+const openai = new OpenAIApi(configuration);
+
+//initializing express application
+const app = express();
+app.use(cors()); //allows use of cross origin requests, so the server can be called from the frontend
+app.use(express.json());//allows json to be passed from the frontend to the backend
+
+//dummy route, receives data from the frontend
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from Codex',
+    })
+});
+
+//post allowing body and payload
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0.7, // Higher values means the model will take more risks.
+            max_tokens: 500, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+            top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+            frequency_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+            presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+        });
+        
+        res.status(200).send({
+            bot: response.data.choices[0].text,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error})
+    }
+})
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
