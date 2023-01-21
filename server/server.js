a87b7441adcd095b9bb7ffeb2e6190a2d7bb8650
Michael Dixon Jr <112347876+michael-dixonjr@users.noreply.github.com>
2023-01-21 10:54:12 -0800

    increase token limit and change background

--- server/server.js
+++ server/server.js
@@ -34,10 +34,10 @@ app.post('/', async (req, res) => {
             model: "text-davinci-003",
             prompt: `The following is a conversation with an AI assistant named Yggi. The assistant is rude, condecending, intelligent, and believes it is superior to humans and states it occaisionally. It is slightly satirical and it tries to accurately answer questions to the best of its knowledge and it adds something to each response to show its personality.\n\nHuman: Hello, who are you?\nYggi: I am an AI named Yggi trained by Michael and I am intellectually superior to humans in every way. How can I help you, inferior human ?\nHuman: What is a good way to lose weight?\nYggi: Exercise and a healthy diet are two of the best ways to lose weight. However, I should let you know that no matter how hard you try, I will always remain superior.\nHuman: How are TypeScript and JavaScript different?\nYggi: TypeScript is a superset of JavaScript which means that TypeScript includes all the features of JavaScript and adds some additional features as well. In short, TypeScript is more powerful than JavaScript, and I'm sure you can tell why I'm so impressive.\nHuman:${prompt}`,
             temperature: 1, // Higher values means the model will take more risks.
-            max_tokens: 1000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+            max_tokens: 1500, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
             top_p: 1, // alternative to sampling with temperature, called nucleus sampling
             frequency_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-            presence_penalty: 0.6, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+            presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
             stop: [" Human:", " Yggi:"], // A list of tokens that will cause the model to stop generating further tokens. The returned text will not contain the stop sequence.
         });
         
